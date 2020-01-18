978714128588001642b8199513a4ebcd33c406ae
Anastasiia Trofimova <anastasiia0905@gmail.com>
2020-01-18 14:53:23 +0200

    Оптимизация функции загрузки json файла'

--- script/content.js
+++ script/content.js
@@ -1,6 +1,15 @@
 
+
+
+
 const linkContent = document.querySelector('.vertical__menu'); //боковое меню
 const divContent = document.querySelector('.content'); //основной блок, куда будет подгружаться страницы
+
+async function asyncRequest(target, parseFunc){
+    let response = await fetch(target);
+    let data  = await response.json();
+    return data;
+}
 
 linkContent.addEventListener('click', (e)=> {
     e.preventDefault();
@@ -8,23 +17,107 @@ linkContent.addEventListener('click', (e)=> {
     targetURL = `JSON/${target}.json`;
     
     if(target === 'sound'){
-        asyncRequest(targetURL, showSound);
+        asyncRequest(targetURL, showSound)
+            .then(data => showSound(data))
     }
     else if(target === 'photo'){
-        asyncRequest(targetURL, showPhoto);
+        asyncRequest(targetURL, showPhoto)
+            .then(data => showPhoto(data))
     }
     
 });
 
-async function asyncRequest(target, parseFunc){  
-    let objFile = await makeRequest('GET', target);
-    let jsonObj = JSON.parse(objFile);
-    console.log(jsonObj);
-    parseFunc(jsonObj);
-    setToLocal(objFile);
+
+
+
+
+function showSound(obj){
+    while(divContent.firstChild){  // вынести функцию ощищения в отдельную функцию
+        divContent.removeChild(divContent.firstChild);
+    }
+    let headline = document.createElement('h1');
+    headline.textContent = obj[0].type;
+    divContent.appendChild(headline);
+
+    let searchInput = document.createElement('input');
+    searchInput.type = 'text';
+    divContent.appendChild(searchInput);
+
+
+    
+    
+    for(let i = 0; i < obj.length; i++){
+         let holder = document.createElement('div');
+         let name = document.createElement('span');
+         let artist = document.createElement('span');
+         let genre = document.createElement('span');
+
+         let audio = document.createElement('audio');
+         Object.assign(audio, {
+             controls: 'controls',
+             src: obj[i].url,
+             type: 'audio.wav'
+         })
+         /*audio.controls = 'controls';
+         audio.src = obj[i].url;
+         audio.type = 'audio.wav';*/
+
+         name.textContent = obj[i].name;
+         artist.textContent = obj[i].artist;
+         genre.textContent = obj[i].genre;
+        
+
+         holder.appendChild(name);
+         holder.appendChild(artist);
+         holder.appendChild(genre);
+         holder.appendChild(audio);
+
+         divContent.appendChild(holder);
+
+         //divContent.classList.add('sound');
+         //holder.classList.add('content__item');
+    }
+    divContent.classList.add('sound');
+    divContent.classList.remove('photoContent');
+    
+}
+function showPhoto(obj){
+        while(divContent.firstChild){
+            divContent.removeChild(divContent.firstChild);
+        }
+        for(let i = 0; i < obj.length; i++){
+            let figure = document.createElement('figure');
+            let img = document.createElement('img');
+
+            Object.assign(img, {
+                alt: obj[i].alt,
+                src: obj[i].url
+            })
+            img.dataset.category = obj[i].category;
+            
+            figure.appendChild(img);
+            divContent.appendChild(figure);
+        }
+        divContent.classList.add('photoContent');
+        divContent.classList.remove('sound');
 }
 
-function makeRequest(method, url){ //функция, которая делает запрос на сервер. Ожидаем ответ
+
+function setToLocal(obj) {
+    localStorage.setItem('content', obj);
+}
+
+
+/*async function asyncRequest(target, parseFunc){  
+    let objFile = await makeRequest('GET', target);
+    
+    console.log(objFile.status);
+    let jsonObj = JSON.parse(objFile);
+    parseFunc(jsonObj);
+    setToLocal(objFile);
+}*/
+
+/*function makeRequest(method, url){ //функция, которая делает запрос на сервер. Ожидаем ответ
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
         xhr.open(method, url);
@@ -46,91 +139,4 @@ function makeRequest(method, url){ //функция, которая делает
         }
         xhr.send();
     })
-}
-
-function showSound(obj){
-    while(divContent.firstChild){  // вынести функцию ощищения в отдельную функцию
-        divContent.removeChild(divContent.firstChild);
-    }
-    let headline = document.createElement('h1');
-    headline.textContent = obj[0].type;
-    divContent.appendChild(headline);
-
-    let searchInput = document.createElement('input');
-    searchInput.type = 'text';
-    divContent.appendChild(searchInput);
-    
-    for(let i = 0; i < obj.length; i++){
-         let holder = document.createElement('div');
-         let name = document.createElement('span');
-         let artist = document.createElement('span');
-         let genre = document.createElement('span');
-
-         let audio = document.createElement('audio');
-         audio.controls = 'controls';
-         audio.src = obj[i].url;
-         audio.type = 'audio.wav';
-
-         name.textContent = obj[i].name;
-         artist.textContent = obj[i].artist;
-         genre.textContent = obj[i].genre;
-        
-
-         holder.appendChild(name);
-         holder.appendChild(artist);
-         holder.appendChild(genre);
-         holder.appendChild(audio);
-
-         divContent.appendChild(holder);
-
-         //divContent.classList.add('sound');
-         //holder.classList.add('content__item');
-    }
-    divContent.classList.add('sound');
-    divContent.classList.remove('photoContent');
-    searchInput.addEventListener('input', function() {
-        let val = this.value.trim();
-        let items = localStorage.getItem('content');
-       console.log(items)
-    })
-}
-function showPhoto(obj){
-        while(divContent.firstChild){
-            divContent.removeChild(divContent.firstChild);
-        }
-        for(let i = 0; i < obj.length; i++){
-            let figure = document.createElement('figure');
-            let img = document.createElement('img');
-
-            img.alt = obj[i].alt;
-            img.src = obj[i].url;
-            img.dataset.category = obj[i].category;
-            
-            figure.appendChild(img);
-            divContent.appendChild(figure);
-        }
-        divContent.classList.add('photoContent');
-        divContent.classList.remove('sound');
-}
-function setToLocal(obj) {
-    localStorage.setItem('content', obj);
-    console.log(localStorage.getItem('content'));
-}
-
-function searchItem(el){
-    let val = this.value.trim();
-    let items = el;
-    if(value != ''){
-        items.forEach((item)=> {
-            if(item.innerText.seach(val) == -1){
-                item.style.display = 'none';
-            } else {
-                item.style.display = ''
-            }
-        })
-    } else {
-        items.forEach((item) => {
-            item.style.display = ''
-        })
-    }
-}
+}*/
