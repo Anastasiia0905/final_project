5db0268f544190a08d62e6a8bfba358dd53f0433
Anastasiia Trofimova <anastasiia0905@gmail.com>
2020-01-28 22:07:07 +0200

    именена структура проекта

--- script/lightbox.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('onload', (e)=> {
-    const click = document.querySelector('.content__download_button');
-click.addEventListener('click', ()=> {
-    console.log(000);
-})
-})
